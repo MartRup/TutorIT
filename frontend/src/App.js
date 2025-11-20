930ea8006193cc749300ee30debcf72abc3c2144
Raymart Ruperez <@Martrup>
2025-11-20 16:39:19 +0800

    feat(pages): implement login page with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TutorITLanding from './components/TutorITLanding';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <TutorITLanding />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<TutorITLanding />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
